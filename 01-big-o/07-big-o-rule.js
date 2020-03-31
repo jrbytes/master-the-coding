bb7c678c2d72461176e0f7a2b57ebf19c4c3d923
JrBytes <itamazor@gmail.com>
2020-03-31 19:55:12 -0300

    Módulo Big O, Rules

--- 01-big-o/07-big-o-rule.js
+++ 01-big-o/07-big-o-rule.js
@@ -1,91 +1,91 @@
-// // Regra 1 :: O Big-O se preocupa com o pior cenário, 
-// // Regra 2 :: Remover constantes
-// // Regra 3 :: Diferentes termos de input, (input1, input2)
-// // Regra 4 :: Descarta a não dominância, termos !dominantes
+// Regra 1 :: O Big-O se preocupa com o pior cenário, 
+// Regra 2 :: Remover constantes
+// Regra 3 :: Diferentes termos de input, (input1, input2)
+// Regra 4 :: Descarta a não dominância, termos !dominantes
 
 
-// // Regra 1
-// const nemo = ['nemo']                                   
-// const everyone = ['dory', 'bruce', 'marlin', 'nemo',
-// 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
-// // Se nemo estivesse no indice 9 do array, seria o pior cado
-// // pq iria percorrer todo o array p chegar nele, essa é a 
-// // primeira regra do Big-O, se preocupar com pior cenário
-// const large = new Array(1000).fill('nemo')              
+// Regra 1
+const nemo = ['nemo']                                   
+const everyone = ['dory', 'bruce', 'marlin', 'nemo',
+'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+// Se nemo estivesse no indice 9 do array, seria o pior cado
+// pq iria percorrer todo o array p chegar nele, essa é a 
+// primeira regra do Big-O, se preocupar com pior cenário
+const large = new Array(1000).fill('nemo')              
 
-// function findNemo(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     if (array[i] === 'nemo') {
-//       console.log('Found NEMO')
-//       break // Tornou o código mais eficiente
-//     }
-//   }
-// }
-// findNemo(everyone)
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemo') {
+      console.log('Found NEMO')
+      break // Tornou o código mais eficiente
+    }
+  }
+}
+findNemo(everyone)
 
 
-// // Regra 2
-// function printFirstItemThenFirstHalfThenSayHi100Times(items) {
-//   console.log(items[0])
+// Regra 2
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+  console.log(items[0])
 
-//   var middleIndex = Math.floor(items.length / 2)
-//   var index = 0
+  var middleIndex = Math.floor(items.length / 2)
+  var index = 0
 
-//   while (index < middleIndex) {
-//     console.log(items[index])
-//     index++
-//   }
+  while (index < middleIndex) {
+    console.log(items[index])
+    index++
+  }
 
-//   for (var i = 0; i < 100; i++) {
-//     console.log('hi')
-//   }
-// }
-// // O(1 + n/2 + 100)
-// // O(n/2 + 101)
-// // O(n/2 + 1)
-// // O(n + 1)
-// // O(n)
+  for (var i = 0; i < 100; i++) {
+    console.log('hi')
+  }
+}
+// O(1 + n/2 + 100)
+// O(n/2 + 101)
+// O(n/2 + 1)
+// O(n + 1)
+// O(n)
 
-// function compressBoxesTwice(boxes) {
-//   boxes.forEach(function(boxes) {
-//     console.log(boxes)
-//   })
+function compressBoxesTwice(boxes) {
+  boxes.forEach(function(boxes) {
+    console.log(boxes)
+  })
   
-//   boxes.forEach(function(boxes) {
-//     console.log(boxes)
-//   })
-// }
-// // O(2n) // Loops se repetindo
-// // O(n)
+  boxes.forEach(function(boxes) {
+    console.log(boxes)
+  })
+}
+// O(2n) // Loops se repetindo
+// O(n)
 
 
-// // Regra 3
-// function compressBoxesTwice(boxes, boxes2) {
-//   boxes.forEach(function(boxes) {
-//     console.log(boxes)
-//   })
+// Regra 3
+function compressBoxesTwice(boxes, boxes2) {
+  boxes.forEach(function(boxes) {
+    console.log(boxes)
+  })
 
-//   boxes2.forEach(function(boxes) {
-//     console.log(boxes)
-//   })
-// }
-// // O(a + b) // Loops diferentes, os args
+  boxes2.forEach(function(boxes) {
+    console.log(boxes)
+  })
+}
+// O(a + b) // Loops diferentes, os args
 
-// // Evitar for dentro de for, é um código horrível
-// // as operações crescem exponencialmente
-// const boxes = [1,2,3,4,5]
+// Evitar for dentro de for, é um código horrível
+// as operações crescem exponencialmente
+const boxes = [1,2,3,4,5]
 
-// function logAllPairsOfArray(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     for (let j = 0; j < array.length; j++) {
-//       console.log(array[i], array[j])
-//     }
-//   }
-// }
-// logAllPairsOfArray(boxes)
+function logAllPairsOfArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      console.log(array[i], array[j])
+    }
+  }
+}
+logAllPairsOfArray(boxes)
 
-// // O(n * n)
-// // O(n^2)
+// O(n * n)
+// O(n^2)
 
 
 // Regra 4
@@ -104,3 +104,6 @@ function printAllNumbersThenAllPairSums(numbers) {
 }
 
 printAllNumbersThenAllPairSums([1,2,3,4,5])
+
+// O(n + n^2)
+// O(n^2)
