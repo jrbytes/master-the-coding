947cbeaab54af80849648137632d3f010b4ca49e
Jr Bytes <itamazor@gmail.com>
2020-07-21 17:02:21 -0300

    Modulo Data Structures, Array Introductions.

---
+++ 03-data-structures-array/01-array-introductions.js
@@ -0,0 +1,12 @@
+const strings = ['a', 'b', 'c', 'd']
+
+strings.push('e') // O(1)
+
+strings.pop()
+strings.pop() // O(1)
+
+strings.unshift('x') // O(n)
+
+strings.splice(2, 0, 'alien') // O(n)
+
+console.log(strings)
