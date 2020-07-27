63c4896900a5b664cacdc85b7611f5c2e3546e8c
Jr Bytes <itamazor@gmail.com>
2020-07-27 15:48:21 -0300

    Modulo Data Structures Array, Implement a Hash Table.

--- 04-data-structures-hash-tables/02-implement-a-hash-table.js
+++ 04-data-structures-hash-tables/02-implement-a-hash-table.js
@@ -33,9 +33,30 @@ class HashTable {
     } // O(1)
     return undefined
   }
+
+  keys() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    for (let i = 0; i < this.data.length; i++) {
+        if (this.data[i] && this.data[i].length) {
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
+  }
 }
 
-const myHashTable = new HashTable(50)
+const myHashTable = new HashTable(5)
 console.log(myHashTable.set('grapes', 10000))
 console.log(myHashTable.set('apples', 54))
-console.log(myHashTable.get('apples'))
+console.log(myHashTable.set('oranges', 2))
+// console.log(myHashTable.get('apples'))
+console.log(myHashTable.keys())
